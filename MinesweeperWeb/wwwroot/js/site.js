b60aae42958d305d6a539f13027e5d5992fe2168
HoangHuy1405 <nguyenmaihoanghuy@gmail.com>
2024-09-25 20:22:23 +0700

    add functionality to populate grid container with 2D array

--- MinesweeperWeb/wwwroot/js/site.js
+++ MinesweeperWeb/wwwroot/js/site.js
@@ -1,25 +1,51 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+
+var gridItems = document.querySelectorAll(".grid-item");
 
 
-// call the function generateBoard to get initial board
+// call the function generateBoard to get initial board stored into a var
+var board = [
+    [9,1,0,0,0,0,0,0],
+    [1,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+]
+let bombInt = 9;
+// function that populate the grid container based on var board
+console.log(board);
+function print() {
 
+}
 // click a cell and reveal the closed cell along with it data?
     // click
-var gridItems = document.querySelectorAll(".grid-item");
-console.log(gridItems);
+
     // check if grid item is a closed cell, which can only be clicked on
 gridItems.forEach((gridItem, index) => {
-    gridItem.addEventListener("click", function () {
-        if (gridItem.classList.contains("closed")) {
-            // clickable
-            console.log(gridItem);
-            gridItem.classList.add("opened");
+    if (gridItem.classList.contains("closed")) {
+        gridItem.addEventListener("click", function () {
             gridItem.classList.remove("closed");
-        }
-    })
+            // clickable
+            var x = Math.floor(index / board.length);
+            var y = index % board.length; 
+
+            if (board[x][y] == 9) {
+                gridItem.classList.add("mine");
+            } else if (board[x][y] == 0) {
+                gridItem.classList.add("opened");
+            } else {
+                gridItem.classList.add("opened");
+                gridItem.innerHTML = board[x][y];
+            }
+            
+            
+        })
+    }
 }) 
 
 
