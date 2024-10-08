39b6e378e9acefc75123f78a1c8b0aab86d688af
HoangHuy1405 <nguyenmaihoanghuy@gmail.com>
2024-10-08 18:06:44 +0700

    .

--- MinesweeperWeb/services/SessionExtensions.cs
+++ MinesweeperWeb/services/SessionExtensions.cs
@@ -1,16 +1,12 @@
 ﻿using System.Text.Json;
 
-namespace MinesweeperWeb.services
-{
-    public static class SessionExtensions
-    {
-        public static void Set<T>(this ISession session, string key, T value)
-        {
+namespace MinesweeperWeb.Services {
+    public static class SessionExtensions {
+        public static void Set<T>(this ISession session, string key, T value) {
             session.SetString(key, JsonSerializer.Serialize(value));
         }
 
-        public static T? Get<T>(this ISession session, string key)
-        {
+        public static T? Get<T>(this ISession session, string key) {
             var value = session.GetString(key);
             return value == null ? default : JsonSerializer.Deserialize<T>(value);
         }
--- MinesweeperWeb/services/Utilities.cs
+++ MinesweeperWeb/services/Utilities.cs
@@ -1,18 +1,18 @@
-﻿namespace MinesweeperWeb.services {
+﻿namespace MinesweeperWeb.Services {
     public static class Utilities {
         public static int[] convertToOneD(int[,] boardArr) {
-            int[] arr = new int[boardArr.GetLength(0)*boardArr.GetLength(1)];
-            for(int i = 0; i < boardArr.GetLength(0); i++) {
-                for(int j = 0; j < boardArr.GetLength(1); j++) {
-                    arr[i * boardArr.GetLength(1) + j] = boardArr[i,j];
+            int[] arr = new int[boardArr.GetLength(0) * boardArr.GetLength(1)];
+            for (int i = 0; i < boardArr.GetLength(0); i++) {
+                for (int j = 0; j < boardArr.GetLength(1); j++) {
+                    arr[i * boardArr.GetLength(1) + j] = boardArr[i, j];
                 }
             }
             return arr;
         }
 
         public static int[,] convertToTwoD(int[] boardArr, int width, int height) {
-            int[,] board = new int[width,height];
-            for(int i = 0; i < boardArr.Length; i++) {
+            int[,] board = new int[height, width];
+            for (int i = 0; i < boardArr.Length; i++) {
                 board[i / width, i % width] = boardArr[i];
             }
             return board;
