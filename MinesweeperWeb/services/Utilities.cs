1cd8ff3019818c9d785c2c1f8f89702f44ef396b
HoangHuy1405 <nguyenmaihoanghuy@gmail.com>
2024-10-08 17:49:02 +0700

    merge

---
+++ MinesweeperWeb/services/Utilities.cs
@@ -0,0 +1,22 @@
+﻿namespace MinesweeperWeb.services {
+    public static class Utilities {
+        public static int[] convertToOneD(int[,] boardArr) {
+            int[] arr = new int[boardArr.GetLength(0)*boardArr.GetLength(1)];
+            for(int i = 0; i < boardArr.GetLength(0); i++) {
+                for(int j = 0; j < boardArr.GetLength(1); j++) {
+                    arr[i * boardArr.GetLength(1) + j] = boardArr[i,j];
+                }
+            }
+            return arr;
+        }
+
+        public static int[,] convertToTwoD(int[] boardArr, int width, int height) {
+            int[,] board = new int[width,height];
+            for(int i = 0; i < boardArr.Length; i++) {
+                board[i / width, i % width] = boardArr[i];
+            }
+            return board;
+        }
+
+    }
+}
